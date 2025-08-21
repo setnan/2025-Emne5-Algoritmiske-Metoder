a1bef2afa3e53bcaf5a051a00ff9260fc02c9461
Simon Etnan <setnan87@hotmail.com>
2025-08-21 11:54:27 +0200

    slutten av dagen med StopWatch

--- Week34/StopWatchTest/Program.cs
+++ Week34/StopWatchTest/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System.Diagnostics;
+using System;
+
+int loopCount = 100_000_000;
+int count = 6;
+
+Console.WriteLine($"Sum of numbers from 1 to {count}: {SumNumbers(count)}");
+
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+
+for (int i = 0; i < loopCount; i++)
+{
+    SumNumbers(count);
+}
+
+stopwatch.Stop();
+Console.WriteLine($"Time taken for {loopCount} iterations: {stopwatch.ElapsedMilliseconds} ms");
+
+int SumNumbers(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
